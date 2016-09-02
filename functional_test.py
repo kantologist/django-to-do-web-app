c10dd152b0352d983d4d2987f3141a5edda10566
azeez_femi <azeezfemi17937@yahoo.com>
2016-09-02 11:49:08 +0100

    refactored checking table

--- functional_test.py
+++ functional_test.py
@@ -12,7 +12,12 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
-        self.browser.quit()    
+        self.browser.quit()   
+
+    def check_for_row_in_list(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])     
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -41,10 +46,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         import time
         time.sleep(10)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list('1: Buy peacock feathers')
+        self.check_for_row_in_list('2: Use peacock feathers to make a fly')
        
         # The page updates again, and now shows both items on her list
         self.fail("Finish the test!")
