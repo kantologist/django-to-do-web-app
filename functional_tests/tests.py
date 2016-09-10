b5508b755bb13cc464f8289f8f78f68c9f7db519
azeez_femi <azeezfemi17937@yahoo.com>
2016-09-10 12:02:15 +0100

    test if different user have differnt url

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -9,7 +9,7 @@ class NewVisitorTest(LiveServerTestCase):
         chromedriver = r'C:\Users\Oluwafemi\Downloads\chromedriver_win32\chromedriver'
         os.environ["webdriver.chrome.driver"] = chromedriver
         self.browser = webdriver.Chrome(chromedriver)
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(1)
 
     def tearDown(self):
         self.browser.quit()
@@ -36,9 +36,11 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-
-         # "1: Buy peacock feathers" as an item in a to-do list
+        edit_list_url = self.browser.current_url
+        self.assertRegexpMatches(edit_list_url,'/lists/.+')
+        self.check_for_row_in_list("1: Buy peacock feathers" )
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox=self.browser.find_element_by_id('id_new_item')
@@ -46,10 +48,41 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         import time
         time.sleep(10)
+        # The page updates again, and now shows both items on her list
         self.check_for_row_in_list('1: Buy peacock feathers')
         self.check_for_row_in_list('2: Use peacock feathers to make a fly')
 
-        # The page updates again, and now shows both items on her list
+        # Now a new user, Francis, comes along to the site.
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc #
+        self.browser.quit()
+        chromedriver = r'C:\Users\Oluwafemi\Downloads\chromedriver_win32\chromedriver'
+        os.environ["webdriver.chrome.driver"] = chromedriver
+        self.browser = webdriver.Chrome(chromedriver)
+
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get_url(live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('1: Buy peacock feathers',page_text)
+        self.assertNotIn('2: Use peacock feathers to make a fly',page_text)
+
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegexpMatches(francis_list_url,'/lists/.+')
+        self.assertNotEqual(francis_list_url,edith_list_url)
+
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body')
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertIn('Buy milk',page_text)
+
+        # Satisfied, they both go back to sleep
         self.fail("Finish the test!")
 #if __name__ == '__main__':
 #    unittest.main()
